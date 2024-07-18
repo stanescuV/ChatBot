d47fca6074b41bf99c09956a4bc4d3b65c17575c
Stanescu Victor <stanescuvictor12@gmail.com>
2024-07-18 13:43:10 +0200

    Everything seems to work

--- chatbot.py
+++ chatbot.py
@@ -70,7 +70,7 @@ clientOpenAI = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
 def get_embedding(text, model="text-embedding-3-large"):
     return clientOpenAI.embeddings.create(input=[text], model=model).data[0].embedding
 
-query = "Ce trebuie sa faca un capitan pe un vapor militar ?"
+query = "Ce se intampla in caz de talharie?"
 queryEmbedding = get_embedding(query)
 # Single vector search
 res = clientMilvus.search(
@@ -85,73 +85,73 @@ res = clientMilvus.search(
 result = json.dumps(res, indent=4)
 print(result)
 
-#
-# with open("codPenal.txt", "r", encoding='utf-8') as file:
-#     codPenal = file.read()
-#
-#
-# #From big String to an array of articles
-#
-# def parse_legal_articles(text):
-#     # Define the pattern
-#     pattern = re.compile(r'(Art\.\s\d+\.)')
-#
-#     # Split the text at each article start
-#     parts = pattern.split(text)[1:]  # [1:] to skip the first empty result
-#
-#     articles = []
-#     skip_keywords = ["CAP", "SEC", "TIT"]
-#
-#     for i in range(0, len(parts), 2):
-#
-#         article_content = parts[i + 1]
-#
-#         for keyword in skip_keywords:
-#             if keyword in article_content:
-#                 keyword_position = article_content.find(keyword)
-#                 article_content = article_content[:keyword_position].strip()
-#                 break
-#
-#         article_content = article_content.replace('\n', ' ')
-#
-#         article_name = article_content.split('.')[0].strip()
-#         article_text = article_content.split('.', 1)[1].strip() if '.' in article_content else ''
-#
-#         articles.append({
-#             'ArtName': article_name,
-#             'ArtText': article_text
-#         })
-#
-#     return articles
-#
-#
-#
+
+with open("codPenal.txt", "r", encoding='utf-8') as file:
+    codPenal = file.read()
+
+
+#From big String to an array of articles
+
+def parse_legal_articles(text):
+    # Define the pattern
+    pattern = re.compile(r'(Art\.\s\d+\.)')
+
+    # Split the text at each article start
+    parts = pattern.split(text)[1:]  # [1:] to skip the first empty result
+
+    articles = []
+    skip_keywords = ["CAP", "SEC", "TIT"]
+
+    for i in range(0, len(parts), 2):
+
+        article_content = parts[i + 1]
+
+        for keyword in skip_keywords:
+            if keyword in article_content:
+                keyword_position = article_content.find(keyword)
+                article_content = article_content[:keyword_position].strip()
+                break
+
+        article_content = article_content.replace('\n', ' ')
+
+        article_name = article_content.split('.')[0].strip()
+        article_text = article_content.split('.', 1)[1].strip() if '.' in article_content else ''
+
+        articles.append({
+            'ArtName': article_name,
+            'ArtText': article_text
+        })
+
+    return articles
+
+
+
 # # INSERT MILVUS
 #
 # #SET UP FOR PROD
-# # embeddings = []
-# # articles = parse_legal_articles(codPenal)
-# #
-# # for ix, article in enumerate(articles) :
-# #     embeddings.append({ "text" : str(article), "embedding" : get_embedding(str(article))})
-# #
-# # print(len(embeddings))
-# #
-# # insertData(db_name, embeddings)
+# embeddings = []
+# articles = parse_legal_articles(codPenal)
+#
+# for ix, article in enumerate(articles) :
+#     embeddings.append({ "text" : str(article), "embedding" : get_embedding(str(article))})
+#
+# print(len(embeddings))
+#
+# insertData(db_name, embeddings)
 #
 #
 # #SET UP FOR TEST
-# # embeddingsTest = []
-# # articlesTest = [articles[0],articles[1]]
-# #
-# # for ix, article in enumerate(articlesTest):
-# #     embeddingsTest.append({ "text" : str(article), "embedding" : get_embedding(str(article))})
-# #
-# # print(len(embeddingsTest))
-# #
-# # insertData(db_name_test, embeddingsTest)
+# embeddingsTest = []
+# articlesTest = [articles[0],articles[1]]
 #
+# for ix, article in enumerate(articlesTest):
+#     embeddingsTest.append({ "text" : str(article), "embedding" : get_embedding(str(article))})
 #
+# print(len(embeddingsTest))
+#
+# insertData(db_name_test, embeddingsTest)
+#
+# #
 
 
 
