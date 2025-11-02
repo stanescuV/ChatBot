92218b61e15a2c9129076888e9eacf49396c868c
Stanescu Victor <stanescuvictor12@gmail.com>
2025-11-02 20:23:06 +0100

    implemented uv and langchain

--- chatbot.py
+++ chatbot.py
@@ -1,27 +1,30 @@
 import os
+from langchain.agents import create_agent
 from dotenv import load_dotenv
-from openai import OpenAI
+from langchain_openai import OpenAIEmbeddings
 from db.milvus_handler import MilvusHandler
-# from test_dataset.intrebari_test import intrebari_test, write_in_csv
-# import numpy as np
+
 
 load_dotenv()
 
-
-#TODO: FUNCTION DISPATCHER
-#TODO: Dataset cu intrebari in multiple feluri, proaste / stupide, cooerente profi, cooerente formulate informal ...etc 
 
 # === Init Milvus ===
 milvus = MilvusHandler()
 
 # EMBEDING / AI
 api_key = os.getenv("OPENAI_API_KEY")
+model = os.getenv("CHATBOT_MODEL_GENERATIVE")
+embedding_model = os.getenv("CHATBOT_MODEL_EMBEDDING")
+embeddings = OpenAIEmbeddings(model=embedding_model)
 
-# Initialize client correctly (must be keyword arg)
-clientOpenAI = OpenAI(api_key=api_key)
-model = "gpt-4o-mini"
+# === Init Agent===
+agent = create_agent(
+    model=model,
+    system_prompt="Eşti un sistem profesionist de avocatură care vorbeşte limba română şi este specializat în Codul Penal şi legislaţia română.",
+)
 
-def get_chatbot_answer(query, articles):
+
+def get_chatbot_answer(query:str, articles:list):
     """
     Generates a concise, professional answer in Romanian to a legal query using provided articles.
 
@@ -32,27 +35,26 @@ def get_chatbot_answer(query, articles):
     Returns:
         str: The chatbot's answer in Romanian.
     """
-    context=str(articles)
-    completion = clientOpenAI.chat.completions.create(
-        model=model,
-        messages=[
-            {
-                "role": "system",
-                "content": "Eşti un sistem profesionist de avocatură care vorbeşte limba română şi este specializat în Codul Penal şi legislaţia română."
-            },
-            {
-                "role": "user", "content": f"""
-                --IDENTITY: A romanian lawyer that answers short and concise.
-                --TASK: You are going to answer the USER_QUERY by using CONTEXT.
-                --USER_QUERY: {query}
-                --CONTEXT: {context}
-                """
-            }
-        ]
+    context = str(articles)
+    completion = agent.invoke(
+        {
+            "messages": [
+                {
+                "role": "user",
+                "content": f"""
+                    --IDENTITY: A romanian lawyer that answers short and concise.
+                    --TASK: You are going to answer the USER_QUERY by using CONTEXT.
+                    --USER_QUERY: {query}
+                    --CONTEXT: {context}
+                """,
+                }
+            ]
+        }
     )
-    return completion.choices[0].message.content
+    return completion["messages"][-1].content
 
-def get_embedding(text, model="text-embedding-3-large"):
+
+def get_embedding(text: str):
     """
     Generates an embedding vector for the given text using OpenAI's embedding model.
 
@@ -63,7 +65,10 @@ def get_embedding(text, model="text-embedding-3-large"):
     Returns:
         list: The embedding vector for the input text.
     """
-    return clientOpenAI.embeddings.create(input=[text], model=model).data[0].embedding
+    return (
+        embeddings.embed_query(text)
+    )
+
 
 def run_chatbot(question: str):
     """
@@ -72,7 +77,7 @@ def run_chatbot(question: str):
     Args:
         question (str): The legal question to be answered.
     """
-    embedding = get_embedding(question, model="text-embedding-3-large")
+    embedding = get_embedding(question)
     context = milvus.search(embedding, top_k=2)
     print(context)
     answer_from_gpt = get_chatbot_answer(question, context)
@@ -80,8 +85,4 @@ def run_chatbot(question: str):
     return answer_from_gpt, context
 
 
-# run_chatbot('ce se intampla daca fur')
-
-
-
-
+run_chatbot("ce se intampla daca fur")
