90839bcaa12e79b730d3f592c39af7b441355f71
stanescuV <stanescuvictor12@gmail.com>
2026-03-23 17:07:08 +0100

    added middleware for cors

--- server.py
+++ server.py
@@ -1,11 +1,19 @@
 from fastapi import FastAPI
-from fastapi.concurrency import run_in_threadpool
+from fastapi.middleware.cors import CORSMiddleware
 from chatbot import run_chatbot
 from pydantic import BaseModel
 
-#uvicorn server:app --reload
+#uv run uvicorn server:app --reload
+#uv run uvicorn server:app --host 0.0.0.0 --port 8000 --reload
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class ChatRequest(BaseModel):
     question: str
